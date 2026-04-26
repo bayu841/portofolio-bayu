4ae7c6e9a62ce622944a7ee266124330220f33d6
Bayu Ardiansyah <bayuardiansyah206@gmail.com>
2026-04-26 14:20:40 +0700

    oke

--- src/sections/HeroSection.vue
+++ src/sections/HeroSection.vue
@@ -51,6 +51,11 @@ const scrollToAbout = () => {
   const el = document.getElementById("about");
   if (el) el.scrollIntoView({ behavior: "smooth" });
 };
+
+const scrollToContact = () => {
+  const el = document.getElementById("contact");
+  if (el) el.scrollIntoView({ behavior: "smooth" });
+};
 </script>
 <template>
   <section
@@ -83,8 +88,10 @@ const scrollToAbout = () => {
           </button>
 
           <button
-            class="border border-gray-600 hover:border-white px-6 py-3 rounded-lg transition">
-            Download CV
+             @click="scrollToContact"
+            class="border border-gray-600 hover:border-white px-6 py-3 rounded-lg transition"
+          >
+            Kontak
           </button>
         </div>
       </div>
